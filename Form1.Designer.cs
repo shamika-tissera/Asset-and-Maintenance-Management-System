31fa4a1c69fd948f394cca42ea38f78caa467d88
Shamika Tissera <nimeshshamika@gmail.com>
2021-06-05 15:46:26 +0530

    Working on startup interface

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace Asset_and_Maintenance_Management_System
+{
+    partial class startup_main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_continue = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_continue
+            // 
+            this.btn_continue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(68)))), ((int)(((byte)(91)))));
+            this.btn_continue.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_continue.Location = new System.Drawing.Point(973, 642);
+            this.btn_continue.Name = "btn_continue";
+            this.btn_continue.Size = new System.Drawing.Size(123, 33);
+            this.btn_continue.TabIndex = 0;
+            this.btn_continue.Text = "Continue";
+            this.btn_continue.UseVisualStyleBackColor = false;
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(68)))), ((int)(((byte)(91)))));
+            this.btn_cancel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_cancel.Location = new System.Drawing.Point(830, 642);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(123, 33);
+            this.btn_cancel.TabIndex = 0;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = false;
+            // 
+            // startup_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(68)))), ((int)(((byte)(91)))));
+            this.ClientSize = new System.Drawing.Size(1136, 708);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_continue);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "startup_main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_continue;
+        private System.Windows.Forms.Button btn_cancel;
+    }
+}
+
